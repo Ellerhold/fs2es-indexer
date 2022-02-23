f9242809aa1f78ff4a60f406e79c7be9555f07e2
Matthias Kühne <matthias.kuehne@ellerhold.de>
2022-02-23 12:43:48 +0100

    Set the version of the elasticsearch lib to 0.7.*

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     ],
     install_requires=[
         'PyYaml',       # Debian 10 Buster: python3-yaml
-        'elasticsearch'
+        'elasticsearch>=0.7.0,<0.8.0'
     ],
     include_package_data=True,
     license='proprietary',
