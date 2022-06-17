6ab6617fa3c38d27a4cc4694857b051b3ba32a7a
Matthias Kühne <matthias.kuehne@ellerhold.de>
2022-06-17 13:32:03 +0200

    publish-pypi.sh

---
+++ publish-pypi.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -lt 3 ];
+then
+    echo "Usage: $0 <Version>"
+    exit 1
+fi
+
+VERSION="$1"
+
+# Write the correct version into the .toml
+sed "+s/{version}/${VERSION}/g" "$1/pyproject.dist.toml" > "$1/pyproject.toml"
