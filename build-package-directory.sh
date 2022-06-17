f81a819d93309f8bd57253f22f7fb9011bd15954
Matthias Kühne <matthias.kuehne@ellerhold.de>
2022-06-17 14:36:21 +0200

    switch to poetry build

--- build-package-directory.sh
+++ build-package-directory.sh
@@ -13,21 +13,23 @@ TARGET="$2"
 VERSION="$3"
 
 # Write the correct version into the .toml
-sed "+s/{version}/${VERSION}/g" "$1/pyproject.dist.toml" > "$1/pyproject.toml"
+sed "+s/{version}/${VERSION}/g" "${SOURCE}/pyproject.dist.toml" > "${SOURCE}/pyproject.toml"
 
-# Create wheels of this project and all its dependencies
-/usr/bin/pip3 wheel "$1" -w "$2/tmp/fs2es-indexer"
+# Create a wheel of this project & publish it to PyPi
+cd "${SOURCE}"
+/usr/local/bin/poetry publish --build
+cp "${SOURCE}/dist/*.whl" "${TARGET}/tmp/fs2es-indexer"
 
-mkdir -p "$2/etc/fs2es-indexer"
-cp "$1/config.dist.yml" "$2/etc/fs2es-indexer/config.yml"
+mkdir -p "${TARGET}/etc/fs2es-indexer"
+cp "${SOURCE}/config.dist.yml" "${TARGET}/etc/fs2es-indexer/config.yml"
 
-mkdir -p "$2/lib/systemd/system"
-cp "$1/fs2es-indexer.service" "$2/lib/systemd/system/fs2es-indexer.service"
+mkdir -p "${TARGET}/lib/systemd/system"
+cp "${SOURCE}/fs2es-indexer.service" "${TARGET}/lib/systemd/system/fs2es-indexer.service"
 
-cp -R "$1/DEBIAN" "$2/DEBIAN"
+cp -R "${SOURCE}/DEBIAN" "${TARGET}/DEBIAN"
 
 # Write the correct version into the postinst
-sed "+s/{version}/${VERSION}/g" "$1/pyproject.dist.toml" > "$1/pyproject.toml"
+sed "+s/{version}/${VERSION}/g" "${SOURCE}/pyproject.dist.toml" > "${SOURCE}/pyproject.toml"
 
-chmod 0755 "$2/DEBIAN/postinst"
-chmod 0755 "$2/DEBIAN/postrm"
+chmod 0755 "${TARGET}/DEBIAN/postinst"
+chmod 0755 "${TARGET}/DEBIAN/postrm"
