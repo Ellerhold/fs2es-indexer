0974b8d1e96c3e4075a70836702ef4e86161434b
Matthias Kühne <matthias.kuehne@ellerhold.de>
2023-03-27 15:02:52 +0200

    save the directories for later 2

--- lib/Fs2EsIndexer.py
+++ lib/Fs2EsIndexer.py
@@ -173,7 +173,7 @@ class Fs2EsIndexer(object):
 
     def index_directories(self, directories):
         """ Imports the content of the directories and all of its sub directories into the elasticsearch index """
-        directories = {}
+        indexed_directories = {}
         documents = []
         documents_indexed = 0
         self.duration_elasticsearch = 0
@@ -208,7 +208,7 @@ class Fs2EsIndexer(object):
                 for name in dirs:
                     full_path = os.path.join(root, name)
                     if self.path_should_be_indexed(full_path):
-                        directories[full_path] = 1
+                        indexed_directories[full_path] = 1
 
                         try:
                             documents.append(self.map_path_to_es_document(full_path, name, index_time))
@@ -246,7 +246,7 @@ class Fs2EsIndexer(object):
             '- Elasticsearch import lasted %.2f minutes.' % (self.duration_elasticsearch / 60)
         )
 
-        print(directories)
+        print(indexed_directories)
 
     def path_should_be_indexed(self, path):
         """ Tests if a specific path (dir or file) should be indexed """
